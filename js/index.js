f15bb30ba7398683d522d5a13a8cb68bfdab6c7f
skino0 <skinogigo@gmail.com>
2019-03-29 12:38:09 +0200

    finished,responsive,tons of new styles added

--- js/index.js
+++ js/index.js
@@ -6,16 +6,10 @@ $(document).ready(function() {
         resolution: 'standard_resolution',
         accessToken: '10603786049.1677ed0.eabf52ac399c4ea7afb829dd850d2fb4',
         sortBy: 'most-recent',
-        template: '<div id="ichover"><div class="instaimg" data-aos="fade-up" data-aos-duration="1200"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid" draggable="false" onmousedown="return false;"/><div class="likes"><i class="fas fa-heart"></i>{{likes}}</div></a></div></div>',
+        template: '<div class="ichover"><div class="instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid" draggable="false" onmousedown="return false;"/><div class="likes"><i class="fas fa-heart"></i>{{likes}}</div></a></div></div>',
     });
     userFeed.run();
 
     
-    $('.gallery').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-        gallery: {
-            enabled: true
-        }
-    });
+
 });
