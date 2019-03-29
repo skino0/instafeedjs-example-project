a13a9df82ec8f1a023d59c0fa6b5aa3bde0f493a
skino0 <skinogigo@gmail.com>
2019-03-29 17:33:12 +0200

    added license comment,instafeed one line

--- js/index.js
+++ js/index.js
@@ -1,15 +1,1 @@
-$(document).ready(function() {
-    var userFeed = new Instafeed({
-        get: 'user',
-        userId: '10603786049',
-        limit: 20,
-        resolution: 'standard_resolution',
-        accessToken: '10603786049.1677ed0.eabf52ac399c4ea7afb829dd850d2fb4',
-        sortBy: 'most-recent',
-        template: '<div class="ichover"><div class="instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid" draggable="false" onmousedown="return false;"/><div class="likes"><i class="fas fa-heart"></i>{{likes}}</div></a></div></div>',
-    });
-    userFeed.run();
-
-    
-
-});
+$(document).ready(function() {var userFeed = new Instafeed({get: 'user',userId: '10603786049',limit: 20,resolution: 'standard_resolution',accessToken: '10603786049.1677ed0.eabf52ac399c4ea7afb829dd850d2fb4',sortBy: 'most-recent',template: '<div class="ichover"><div class="instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid" draggable="false" onmousedown="return false;"/><div class="likes"><i class="fas fa-heart"></i>{{likes}}</div></a></div></div>',});userFeed.run();});
